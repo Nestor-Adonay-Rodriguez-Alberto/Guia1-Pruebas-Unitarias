ad3e171ca62023f02f5761b325b23f03df49afac
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-10-08 10:11:31 -0600

    Creacion de las Clases a Testear con pruebas unitarias

---
+++ NARA.Cadena/StringManipulator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace NARA.Cadena
+{
+    public class StringManipulator
+    {
+        // METODO #1:
+        public string ReverseString(string input)
+        {
+            char[] caracteres = input.ToCharArray();
+
+            Array.Reverse(caracteres);
+
+            string inputInvertido = new string(caracteres);
+
+            return inputInvertido;
+        }
+
+        // METODO #2:
+        public string RemoveSpaces(string input)
+        {
+            string inputSinEspacios = input.Replace(" ", "");
+
+            return inputSinEspacios;
+        }
+    }
+}
---
+++ NARA.Temperatura/TemperatureConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace NARA.Temperatura
+{
+    public class TemperatureConverter
+    {
+
+        // METODO #1:
+        public double CelsiusToFahrenheit(double celsius)
+        {
+            // Formula: F=( C * 1.8 ) + 32
+
+            double Fahrenheit = (celsius * 1.8) + 32;
+
+            return Fahrenheit;
+        }
+
+        // METODO #2:
+        public double FahrenheitToCelsius(double fahrenheit)
+        {
+            // Formula: C=( F - 32 ) / 1.8
+
+            double Celsius = (fahrenheit - 32) / 1.8;
+
+            return Celsius;
+        }
+    }
+}
