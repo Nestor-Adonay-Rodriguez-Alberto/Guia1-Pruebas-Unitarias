1027c086a5cf2384f91b1574f74e400b5590cc9e
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-10-08 10:52:27 -0600

    Creacion de las pruebas unitarias

---
+++ NARA.Cadena.UnitTestxUnit/StringManipulatorTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NARA.Cadena.UnitTestxUnit
+{
+    public class StringManipulatorTest
+    {
+        [Fact]
+        public void ReverseStringTest()
+        {
+            // Arange:
+            StringManipulator stringManipulator = new StringManipulator();
+            string input = "Hola Mundo";
+            string Esperado = "odnuM aloH";
+
+            // Act:
+            string inputInvertido = stringManipulator.ReverseString(input);
+
+            // Assert:
+            Assert.Equal(Esperado, inputInvertido);
+        }
+
+
+        [Fact]
+        public void RemoveSpaces()
+        {
+            // Arange:
+            StringManipulator stringManipulator = new StringManipulator();
+            string input = "Hola Mundo Como Estan ?";
+            string Esperado = "HolaMundoComoEstan?";
+
+            // Act:
+            string inputSinEspacios = stringManipulator.RemoveSpaces(input);
+
+            // Assert:
+            Assert.Equal(Esperado, inputSinEspacios);
+
+        }
+    }
+}
---
+++ NARA.Temperatura.UnitTestMSTest/TemperatureConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NARA.Temperatura.UnitTestMSTest
+{
+    [TestClass]
+    public class TemperatureConverterTests
+    {
+
+        // TEST #1:
+        [TestMethod]
+        public void CelsiusToFahrenheitTest()
+        {
+            // Arange:
+            TemperatureConverter converter = new TemperatureConverter();
+            double Celsius = 20;
+            double Esperado = 68;
+
+            // Act:
+            double Fahrenheit = converter.CelsiusToFahrenheit(Celsius);
+
+            // Assert:
+            Assert.AreEqual(Esperado, Fahrenheit);
+        }
+
+
+        // TEST #2:
+        [TestMethod]
+        public void FahrenheitToCelsiusTest()
+        {
+            // Arange:
+            TemperatureConverter converter = new TemperatureConverter();
+            double Fahrenheit = 68;
+            double Esperado = 20;
+
+            // Act:
+            double Celsius = converter.FahrenheitToCelsius(Fahrenheit);
+
+            // Assert:
+            Assert.AreEqual(Esperado, Celsius);
+        }
+
+    }
+}
